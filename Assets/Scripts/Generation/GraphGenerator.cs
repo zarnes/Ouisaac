137e615c7d5a22c44c53bfb53203431cb0a82955
zarnes <zarnes@live.com>
2020-01-13 11:40:42 +0100

    Gné 3

--- Assets/Scripts/Generation/GraphGenerator.cs
+++ Assets/Scripts/Generation/GraphGenerator.cs
@@ -2,167 +2,172 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GraphGenerator : MonoBehaviour
+namespace Ouisaac
 {
-    public Vector2 Scale;
-    public int Seed;
-    public int Rooms = 10;
-    public List<Node> Nodes = new List<Node>();
-
-    private System.Random rnd;
-
-    // Start is called before the first frame update
-    void Start()
+    public class GraphGenerator : MonoBehaviour
     {
-        rnd = new System.Random(Seed);
-        Node first = new Node();
-        first.X = first.Y = 0;
-        Nodes.Add(first);
+        public Vector2 Scale;
+        public int Seed;
+        public int Rooms = 10;
+        public List<Node> Nodes = new List<Node>();
 
-        CreatePath(first, Rooms - 1);
-        //CreatePath(Nodes[5], 3);
-        CreatePath(Nodes[2], 5);
-    }
+        private System.Random rnd;
+        private RoomsManager roomsManager;
 
-    void CreatePath(Node start, int length)
-    {
-        Node parent = start;
-
-        int i = length;
-        while (i > 0)
+        // Start is called before the first frame update
+        void Start()
         {
-            if (i != length)
-                parent = Nodes[Nodes.Count - 1];
+            rnd = new System.Random(Seed);
+            roomsManager = GetComponent<RoomsManager>();
 
-            int maxRand = 0;
-            for (int randI = 0; randI < 4; ++randI)
-                maxRand += parent.weights[randI];
+            Node first = new Node();
+            first.X = first.Y = 0;
+            Nodes.Add(first);
 
-            int rand = rnd.Next(maxRand);
-            Node newNode = null;
+            CreatePath(first, Rooms - 1);
+            //CreatePath(Nodes[5], 3);
+            CreatePath(Nodes[2], 5);
+        }
 
-            for (int directionIdx = 0; directionIdx < 4; ++directionIdx)
+        void CreatePath(Node start, int length)
+        {
+            Node parent = start;
+
+            int i = length;
+            while (i > 0)
             {
-                if (rand <= parent.weights[directionIdx])
+                if (i != length)
+                    parent = Nodes[Nodes.Count - 1];
+
+                int maxRand = 0;
+                for (int randI = 0; randI < 4; ++randI)
+                    maxRand += parent.weights[randI];
+
+                int rand = rnd.Next(maxRand);
+                Node newNode = null;
+
+                for (int directionIdx = 0; directionIdx < 4; ++directionIdx)
                 {
-                    newNode = SpawnNode(parent, (Direction)directionIdx);
-                    if (newNode != null)
+                    if (rand <= parent.weights[directionIdx])
                     {
-                        Nodes.Add(newNode);
-                        --i;
-                        break;
+                        newNode = SpawnNode(parent, (Direction)directionIdx);
+                        if (newNode != null)
+                        {
+                            Nodes.Add(newNode);
+                            --i;
+                            break;
+                        }
+                        else
+                        {
+                            parent.weights[directionIdx] = 0;
+                        }
                     }
-                    else
-                    {
-                        parent.weights[directionIdx] = 0;
-                    }
+                    rand -= parent.weights[directionIdx];
                 }
-                rand -= parent.weights[directionIdx];
             }
         }
-    }
 
-    Node SpawnNode(Node parent, Direction direction)
-    {
-        int X = parent.X;
-        int Y = parent.Y;
-
-        switch (direction)
+        Node SpawnNode(Node parent, Direction direction)
         {
-            case Direction.up:
-                --Y;
-                break;
-            case Direction.right:
-                ++X;
-                break;
-            case Direction.down:
-                ++Y;
-                break;
-            case Direction.left:
-                --X;
-                break;
-        }
+            int X = parent.X;
+            int Y = parent.Y;
 
-        Node alreadyNode = Nodes.Find(n => n.X == X && n.Y == Y);
-        if (alreadyNode != null)
-            return null;
-
-        Node newNode = new Node();
-        newNode.X = X;
-        newNode.Y = Y;
-        newNode.Parent = parent;
-
-        switch (direction)
-        {
-            case Direction.up:
-                parent.directions[0] = newNode.directions[0] = true;
-                break;
-            case Direction.right:
-                parent.directions[1] = newNode.directions[1] = true;
-                break;
-            case Direction.down:
-                parent.directions[2] = newNode.directions[2] = true;
-                break;
-            case Direction.left:
-                parent.directions[3] = newNode.directions[3] = true;
-                break;
-        }
-
-        return newNode;
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        foreach (Node node in Nodes)
-        {
-            Gizmos.color = Color.white;
-            float X = node.X * Scale.x;
-            float Y = node.Y * Scale.y;
-            //Gizmos.DrawWireSphere(new Vector3(X, Y), .2f);
-            Gizmos.DrawWireCube(new Vector3(X, Y), new Vector3(Scale.x * .9f, Scale.y * .9f));
-
-            if (node.Parent != null)
+            switch (direction)
             {
-                float parentX = node.Parent.X * Scale.x;
-                float parentY = node.Parent.Y * Scale.y;
-
-                Gizmos.DrawLine(new Vector3(X, Y), new Vector3(parentX, parentY));
+                case Direction.up:
+                    --Y;
+                    break;
+                case Direction.right:
+                    ++X;
+                    break;
+                case Direction.down:
+                    ++Y;
+                    break;
+                case Direction.left:
+                    --X;
+                    break;
             }
+
+            Node alreadyNode = Nodes.Find(n => n.X == X && n.Y == Y);
+            if (alreadyNode != null)
+                return null;
+
+            Node newNode = new Node();
+            newNode.X = X;
+            newNode.Y = Y;
+            newNode.Parent = parent;
+
+            switch (direction)
+            {
+                case Direction.up:
+                    parent.directions[0] = newNode.directions[0] = true;
+                    break;
+                case Direction.right:
+                    parent.directions[1] = newNode.directions[1] = true;
+                    break;
+                case Direction.down:
+                    parent.directions[2] = newNode.directions[2] = true;
+                    break;
+                case Direction.left:
+                    parent.directions[3] = newNode.directions[3] = true;
+                    break;
+            }
+
+            return newNode;
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            foreach (Node node in Nodes)
+            {
+                Gizmos.color = Color.white;
+                float X = node.X * Scale.x;
+                float Y = node.Y * Scale.y;
+                Gizmos.DrawWireCube(new Vector3(X, Y), new Vector3(Scale.x * .9f, Scale.y * .9f));
+
+                if (node.Parent != null)
+                {
+                    float parentX = node.Parent.X * Scale.x;
+                    float parentY = node.Parent.Y * Scale.y;
+
+                    Gizmos.DrawLine(new Vector3(X, Y), new Vector3(parentX, parentY));
+                }
+            }
+        }
+
+        public enum Direction
+        {
+            up = 0,
+            right = 1,
+            down = 2,
+            left = 3
         }
     }
 
-    public enum Direction
+    [System.Serializable]
+    public class Node
     {
-        up = 0,
-        right = 1,
-        down = 2,
-        left = 3
-    }
-}
+        public int X = 0;
+        public int Y = 0;
 
-[System.Serializable]
-public class Node
-{
-    public int X = 0;
-    public int Y = 0;
+        /*public bool up = false;
+        public bool right = false;
+        public bool down = false;
+        public bool left = false;*/
 
-    /*public bool up = false;
-    public bool right = false;
-    public bool down = false;
-    public bool left = false;*/
+        public bool[] directions = new bool[4];
 
-    public bool[] directions = new bool[4];
+        //public int pUp, pRight, pDown, pLeft = 25;
+        public int[] weights = new int[4];
+        public Node Parent = null;
 
-    //public int pUp, pRight, pDown, pLeft = 25;
-    public int[] weights = new int[4];
-    public Node Parent = null;
-
-    public Node()
-    {
-        for (int i = 0; i < 4; ++i)
+        public Node()
         {
-            directions[i] = false;
-            weights[i] = 25;
+            for (int i = 0; i < 4; ++i)
+            {
+                directions[i] = false;
+                weights[i] = 25;
+            }
         }
     }
 }
---
+++ Assets/Scripts/Generation/RoomsManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ouisaac
+{
+    public class RoomsManager : MonoBehaviour
+    {
+        public List<Room> Rooms;
+
+        public Room Find(System.Random rnd, bool top = false, bool right = false, bool down = false, bool left = false)
+        {
+            List<Room> possibleRooms = new List<Room>();
+            foreach(Room room in Rooms)
+            {
+                if (top && room.TopDoor == Room.DoorPossibility.Never)
+                    continue;
+                if (!top && room.TopDoor == Room.DoorPossibility.Always)
+                    continue;
+
+                if (right && room.RightDoor == Room.DoorPossibility.Never)
+                    continue;
+                if (!right && room.RightDoor == Room.DoorPossibility.Always)
+                    continue;
+
+                if (down && room.DownDoor == Room.DoorPossibility.Never)
+                    continue;
+                if (!down && room.DownDoor == Room.DoorPossibility.Always)
+                    continue;
+
+                if (left && room.LeftDoor == Room.DoorPossibility.Never)
+                    continue;
+                if (!left && room.LeftDoor == Room.DoorPossibility.Always)
+                    continue;
+
+                possibleRooms.Add(room);
+            }
+
+            if (possibleRooms.Count == 0)
+                return null;
+
+            int rand = rnd.Next(possibleRooms.Count);
+            return possibleRooms[rand];
+        }
+    }
+
+    [System.Serializable]
+    public class Room
+    {
+        public GameObject Prefab;
+        public DoorPossibility TopDoor;
+        public DoorPossibility RightDoor;
+        public DoorPossibility DownDoor;
+        public DoorPossibility LeftDoor;
+
+        public enum DoorPossibility
+        {
+            Possible,
+            Always,
+            Never,
+        }
+    }
+}
