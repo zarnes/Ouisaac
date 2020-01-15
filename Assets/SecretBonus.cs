02515ebe0148d0c192e14c5d49c40604851efb40
LAPTOP-JVJN19AV\flore <florentbbn@gmail.com>
2020-01-15 11:41:51 +0100

    secret room

---
+++ Assets/SecretBonus.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecretBonus : MonoBehaviour
+{
+
+    public Sprite bonusSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (Player.Instance == null)
+            return;
+        if (collision.attachedRigidbody.gameObject != Player.Instance.gameObject)
+            return;
+
+
+        Player.Instance.gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = bonusSprite;
+        Player.Instance.gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.white;
+
+        Destroy(gameObject.transform);
+
+
+    }
+}
